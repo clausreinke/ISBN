184aee5d0e12d6c52cfab428ef4b5f43309aad3f
Claus Reinke <whycombinator@gmail.com>
2015-05-11 23:12:54 +0200

    add isbn lookup by worldcat.org

--- www/js/app.js
+++ www/js/app.js
@@ -2,26 +2,51 @@
 
 angular.module('app', ['ionic'])
 
-.controller('ISBNCtrl',function($scope){
+.controller('ISBNCtrl',function($scope,$http){
+
+  $scope.itemFields = ["isbn","title","author","publisher","year"];
+
   $scope.items = {
     '0': {title:'The Title',author:'An Author'}
    ,'1': {title:'The Sequel',author:'Another Author'}
   };
 
-  $scope.item = {};
+  $scope.item = {isbn:9783446439412};
+
+  $scope.message = '';
 
   $scope.clear = function(){
     console.log("$scope.clear");
-    $scope.item = { isbn: '', author: '', title: ''};
+    $scope.item = { };
   };
 
   $scope.lookup = function(){
     console.log("$scope.lookup");
+    if ($scope.item.isbn) {
+      var url = 'http://xisbn.worldcat.org/webservices/xid/isbn/'
+                +$scope.item.isbn+'?method=getMetadata&format=json&fl=*&callback=JSON_CALLBACK'
+      $http.jsonp(url)
+           .then(function(response){
+                   $scope.response=response;
+                   var result = response.data.list[0];
+                   $scope.itemFields.forEach(function(field){
+                     $scope.item[field] = result[field];
+                   });
+                   $scope.message = undefined;
+                 }
+                ,function(response){
+                   $scope.response=response;
+                   $scope.message = 'lookup failed';
+                 }
+                );
+    } else {
+      $scope.message = 'no ISBN for lookup';
+    }
   };
 
-  $scope.byISBN = function($event){
-    console.log("$scope.byISBN",$scope.item);
-    if ($event.which===13) {
+  $scope.byField = function(field,$event){
+    console.log("$scope.byField",field,$scope.item);
+    if (field==="isbn" && $event.which===13) {
       var storedItem = $scope.items[$scope.item.isbn];
       if (storedItem) {
         console.log(storedItem);
