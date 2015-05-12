3aa1d855f990b7dc9a83a101a3e51c548b4d5f58
Claus Reinke <whycombinator@gmail.com>
2015-05-12 23:02:15 +0200

    add temporary item storage and lookup by field match

--- www/js/app.js
+++ www/js/app.js
@@ -4,20 +4,34 @@ angular.module('app', ['ionic'])
 
 .controller('ISBNCtrl',function($scope,$http){
 
-  $scope.itemFields = ["isbn","title","author","publisher","year"];
+  $scope.itemFields = ["isbn","title","author","publisher","ed","year","lang","info"];
 
-  $scope.items = {
-    '0': {title:'The Title',author:'An Author'}
-   ,'1': {title:'The Sequel',author:'Another Author'}
-  };
+  function copyItem(target,source) {
+    $scope.itemFields.forEach(function(field){
+      target[field] = source[field];
+    });
+    return target;
+  }
 
   $scope.item = {isbn:9783446439412};
 
   $scope.message = '';
 
+  $scope.items = {
+    '0': {isbn: '0', title: 'The Title', author: 'An Author'}
+   ,'1': {isbn: '1', title: 'The Sequel', author: 'Another Author'}
+  };
+
+  $scope.store = function() {
+    console.log("$scope.store");
+    $scope.items[$scope.item.isbn] = copyItem({},$scope.item);
+  };
+
   $scope.clear = function(){
     console.log("$scope.clear");
     $scope.item = { };
+    $scope.message = undefined;
+    $scope.response = undefined;
   };
 
   $scope.lookup = function(){
@@ -29,9 +43,8 @@ angular.module('app', ['ionic'])
            .then(function(response){
                    $scope.response=response;
                    var result = response.data.list[0];
-                   $scope.itemFields.forEach(function(field){
-                     $scope.item[field] = result[field];
-                   });
+                   result.isbn = $scope.item.isbn;
+                   copyItem($scope.item,result);
                    $scope.message = undefined;
                  }
                 ,function(response){
@@ -46,12 +59,21 @@ angular.module('app', ['ionic'])
 
   $scope.byField = function(field,$event){
     console.log("$scope.byField",field,$scope.item);
-    if (field==="isbn" && $event.which===13) {
-      var storedItem = $scope.items[$scope.item.isbn];
-      if (storedItem) {
-        console.log(storedItem);
-        $scope.item.title = storedItem.title;
-        $scope.item.author = storedItem.author;
+    if ($event.which===13) {
+      if (field==="isbn") {
+        var storedItem = $scope.items[$scope.item.isbn];
+        if (storedItem) {
+          console.log(storedItem);
+          copyItem($scope.item,storedItem);
+        }
+      } else {
+        var filteredItem = Object.keys($scope.items)
+                                 .filter(function(isbn){
+                                          return $scope.items[isbn][field]
+                                              && $scope.items[isbn][field].match($scope.item[field])
+                                         })[0];
+        console.log(field,$scope.item[field],filteredItem);
+        filteredItem && copyItem($scope.item,$scope.items[filteredItem]);
       }
     }
   };
